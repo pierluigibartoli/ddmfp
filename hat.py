7cd773e2c9ca18aab0d3c9699c34f0c0ef670264
pierluigibartoli <pierluigi.bartoli@gmail.com>
2025-10-20 11:11:31 +0200

    Update hat.py

--- hat.py
+++ hat.py
@@ -10,8 +10,8 @@ import time
 import html
 from urllib.parse import urljoin
 
-MFPLINK = "LINKMFP"
-MFPPSW = "MFPPSW"
+MFPLINK = "https://pigi87-um191025.hf.space"     # non mettere lo / finale al link
+MFPPSW = "13021987"
 # Funzioni dal tuo mpd_decoder.py
 def extract_mpd_link_from_page(url):
     """Estrae il link MPD da una pagina HTML che contiene un iframe con player.html#"""
